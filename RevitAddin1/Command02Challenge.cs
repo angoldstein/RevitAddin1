7f0dca8df47e03e0c1e124fc27d68b3bea59f91e
Andrea Goldstein <agoldstein@pesengineers.com>
2022-07-08 17:06:19 -0400

    session2 challenge

---
+++ RevitAddin1/Command02Challenge.cs
@@ -0,0 +1,84 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Excel = Microsoft.Office.Interop.Excel;
+
+#endregion
+
+namespace RevitAddin1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command02Challenge : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            string excelfile = @"L:\COMMITTEES\Revit\Development\2022 ArchSmarter Add-in Academy\Session02_Challenge.xlsx";
+
+            Excel.Application excelapp = new Excel.Application();
+            Excel.Workbook excelWB = excelapp.Workbooks.Open(excelfile);
+            Excel.Worksheet excelWSlevel = excelWB.Worksheets.Item[1];
+            Excel.Worksheet excelWSsheet = excelWB.Worksheets.Item[2];
+
+            Excel.Range excelRnglevel = excelWSlevel.UsedRange;
+            int rowcount = excelRnglevel.Rows.Count;
+
+            //do some stuff in Excel
+            List<string[]> dataList = new List<string[]>();
+
+            for(int i = 2; i <= rowcount; i++)
+            {
+                Excel.Range levelname = excelWSlevel.Cells[i, 1];
+                Excel.Range levelelev = excelWSlevel.Cells[i, 2];
+
+                string dataname = levelname.Value.ToString();
+                double dataelev = levelelev.Value;
+
+                Excel.Range sheetnum = excelWSsheet.Cells[i, 1];
+                Excel.Range sheetname = excelWSsheet.Cells[i, 2];
+
+                string datashname = sheetname.Value.ToString();
+                string datashnum = sheetnum.Value.ToString();
+
+         
+                using (Transaction t = new Transaction(doc))
+                {
+                    t.Start("Create Levels and Sheets");
+
+                    Level curLevel = Level.Create(doc, dataelev);
+                    curLevel.Name = dataname;
+
+                    FilteredElementCollector collector = new FilteredElementCollector(doc);
+                    collector.OfCategory(BuiltInCategory.OST_TitleBlocks);
+                    collector.WhereElementIsElementType();
+
+                    ViewSheet curSheet = ViewSheet.Create(doc, collector.FirstElementId());
+                    curSheet.SheetNumber = datashnum;
+                    curSheet.Name = datashname;
+
+                    t.Commit();
+                }
+            }
+                   
+         
+
+            excelWB.Close();
+            excelapp.Quit();
+
+            return Result.Succeeded;
+        }
+    }
+}
